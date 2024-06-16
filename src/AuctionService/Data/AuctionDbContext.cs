5672e0fb8d2a299377de926b2de5d74e7cf1d88a
Felipe Andres Ortiz Alvarez <me@felipealvarez.dev>
2024-06-16 13:15:21 -0400

    - add db context

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data
+{
+    public class AuctionDbContext : DbContext
+    {
+        public AuctionDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Auction> Auctions { get; set; }
+        public DbSet<Item> Items { get; set; }
+        
+    }
+}
