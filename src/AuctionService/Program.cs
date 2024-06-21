741bdb7aa9249d49244662f500792292e096c532
Felipe Andres Ortiz Alvarez <me@felipealvarez.dev>
2024-06-20 22:44:19 -0400

    - Se añade inicializador de data en context

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AuctionDbContext>(opt =>
 {
-    opt.UseNpgsql(builder.Configuration.GetConnectionString(""));
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
 });
 
 var app = builder.Build();
@@ -15,5 +15,13 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
